ea056dbe647c87f98e9060640011bd8738b5e3c0
scottdiemer <sdiemer@cfl.rr.com>
2020-10-25 01:12:29 -0400

    connected to local mongodb and moved routes to seperate file

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,24 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// bring routers
+const blogRoutes = require("./routes/blog");
 
 // app
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
 
 // middlewares
 app.use(morgan("dev"));
@@ -17,10 +31,8 @@ if (process.env.NODE_ENV === "development") {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-// routes
-app.get("/api", (req, res) => {
-  res.json({ time: Date().toString() });
-});
+// routes middleware
+app.use("/api", blogRoutes);
 
 // port
 const port = process.env.PORT || 8000;
