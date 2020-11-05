a661759419bb1816cc6c3e6a66b2307cfd73fdf9
scottdiemer <sdiemer@cfl.rr.com>
2020-11-04 20:49:35 -0500

    setup auth and admin middlewares

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 // bring routers
 const blogRoutes = require("./routes/blog");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 // app
 const app = express();
@@ -35,6 +36,7 @@ if (process.env.NODE_ENV === "development") {
 // routes middleware
 app.use("/api", blogRoutes);
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 // port
 const port = process.env.PORT || 8000;
